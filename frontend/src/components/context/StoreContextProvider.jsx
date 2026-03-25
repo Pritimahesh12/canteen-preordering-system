5630fb418673723c98a9197404946889906276c6
pritimahesh <pritimahesh52@gmail.com>
2026-03-25 13:52:19 +0530

    connect frontend and backend

--- frontend/src/components/context/StoreContextProvider.jsx
+++ frontend/src/components/context/StoreContextProvider.jsx
@@ -4,7 +4,7 @@ import { StoreContext } from "./StoreContext";
 
 const StoreContextProvider = (props) => {
     const [cartItems, setCartItems] = useState({});
-    const url = "http://localhost:4000"
+    const url = "https://canteen-preordering-system.onrender.com"
     const [token, setToken] = useState("");
     const [showLogin, setShowLogin] = useState(false);
     const [food_list, setFoodList] = useState([]);
